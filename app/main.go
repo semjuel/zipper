e7459e3a189a8ea6fc71c12cdf16a5fe785ab9c3
Igor Semenyuk <igor.semenyuk@rolique.io>
2017-09-05 00:05:24 +0300

    Add zipper files

---
+++ app/main.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"archive/zip"
+	"io"
+	"database/sql"
+	"time"
+	"encoding/json"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Global sql.DB to access the database by all handlers.
+var db *sql.DB
+type S3Files struct {
+	Name string
+	Path string
+	Type string
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":"+strconv.Itoa(8088), nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Get "r" URL params
+	refs, ok := r.URL.Query()["r"]
+	if !ok || len(refs) < 1 {
+		http.Error(w, "Wrong request. Can not find required param. ", 500)
+		return
+	}
+	ref := refs[0]
+
+	// Create an sql.DB and check for errors.
+	var config = getSQLConnect()
+	db, err := sql.Open("mysql", config)
+	if err != nil {
+		log.Printf(err.Error())
+		http.Error(w, err.Error(), 500)
+	}
+	defer db.Close()
+
+	var data string
+
+	date := int32(time.Now().Unix())
+
+	err = db.QueryRow("SELECT data FROM dc_hashes WHERE hash=? AND expire>?", ref, date).Scan(&data)
+	errorLog(err)
+
+	if len(data) == 0 {
+		http.Error(w, "Wrong request. Nothing found.", 500)
+		return
+	}
+
+	var m []S3Files
+	b := []byte(data)
+	err = json.Unmarshal(b, &m)
+	errorLog(err)
+
+	// @TODO change credentials.
+	// Create the S3 service client for the target region
+	// https://docs.aws.amazon.com/sdk-for-go/v1/developer-guide/configuring-sdk.html
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("eu-central-1"),
+		Credentials: credentials.NewSharedCredentials("", "test-account"),
+	})
+	errorLog(err)
+
+	svc := s3.New(sess)
+	bucket := "schleniger-files"
+
+	// Start processing the response
+	w.Header().Add("Content-Disposition", "attachment; filename=\"download.zip\"")
+	w.Header().Add("Content-Type", "application/zip")
+
+	// Loop over files, add them to the
+	zipWriter := zip.NewWriter(w)
+
+	for _, file := range m {
+		if file.Path == "" {
+			log.Printf("Missing path for file: %v", file)
+			continue
+		}
+
+		// Read file from S3, log any errors.
+		result, err := svc.GetObject(&s3.GetObjectInput{
+			Bucket: aws.String(bucket),
+			Key:    aws.String(file.Path),
+		})
+		if err != nil {
+			log.Printf("Error downloading \"%s\" - %s", file.Path, err.Error())
+			continue
+		}
+
+		defer result.Body.Close()
+
+		// @TODO change zipPath
+		zipPath := file.Name
+
+		// We have to set a special flag so zip files recognize utf file names
+		// See http://stackoverflow.com/questions/30026083/creating-a-zip-archive-with-unicode-filenames-using-gos-archive-zip
+		h := &zip.FileHeader{
+			Name:   zipPath,
+			Method: zip.Deflate,
+			Flags:  0x800,
+		}
+
+		f, _ := zipWriter.CreateHeader(h)
+
+		io.Copy(f, result.Body)
+		result.Body.Close()
+	}
+
+	zipWriter.Close()
+}
+
+func getSQLConnect() string {
+	user := os.Getenv("MYSQL_DB_USER")
+	if len(user) == 0 {
+		// Default value.
+		user = "zipper_user"
+	}
+
+	pass := os.Getenv("MYSQL_DB_PASS")
+	if len(pass) == 0 {
+		// Default value.
+		pass = "zipper_pass"
+	}
+
+	host := os.Getenv("MYSQL_DB_HOST")
+	if len(host) == 0 {
+		// Default value.
+		host = "mariadb"
+	}
+
+	port := os.Getenv("MYSQL_DB_PORT")
+	if len(port) == 0 {
+		// Default value.
+		port = "3306"
+	}
+
+	database := os.Getenv("MYSQL_DATABASE")
+	if len(database) == 0 {
+		// Default value.
+		database = "zipper_db"
+	}
+
+	// Default value to return: drupal:drupal@tcp(mariadb:3306)/drupal"
+	return user + ":" + pass + "@tcp(" + host + ":" + port + ")/" + database
+}
+
+func errorLog(err error) {
+	if err != nil {
+		log.Printf(err.Error())
+	}
+}
